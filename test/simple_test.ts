26b426cabe1609dc0dfdae256e39da629412f57d
Scott Cain <scott+git@scottcain.net>
2023-04-10 12:52:27 -0700

    cleaning up tests but they still don't work

--- test/simple_test.ts
+++ test/simple_test.ts
@@ -1,17 +1,16 @@
 //@ts-nocheck
+import { accessStore } from '../src/seqpanel-api.ts'
 
-import { accessStore , accessFasta ,  assembleBundle } from ../src/seqpanel-api.ts;
+const props = {nclistbaseurl: "https://s3.amazonaws.com/agrjbrowse/MOD-jbrowses/WormBase/WS287/c_elegans_PRJNA13758/",
+            urltemplate: "tracks/Curated_Genes/{refseq}/trackData.jsonz",
+            refseq: "I",
+            start: "43730",
+            end: "44687",
+            gene: "WBGene00022275",
+            transcript: "Y74C9A.1.1"};
 
 describe ( "small worm gene" , () => {
-	const feature = await accessStore ( props: {
-	    nclistbaseurl: "https://s3.amazonaws.com/agrjbrowse/MOD-jbrowses/WormBase/WS287/c_elegans_PRJNA13758/",
-	    urltemplate: "tracks/Curated_Genes/{refseq}/trackData.jsonz",
-	    refseq: "I",
-	    start: 43730, //purposely not quite right
-	    end: 44687,
-	    gene: "WBGene00022275", // txt-7
-	    transcript: "Y74C9A.1.1"
-	}  );
+	const feature = await accessStore ( props );
 
 	expect(feature.length).toBe(1)
         expect(feature[0].get('start')).toBe(43733)
