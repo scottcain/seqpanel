c1b2801130dc801a022101d4b2f01277a3adbe5a
Scott Cain <scott+git@scottcain.net>
2023-04-28 09:46:28 -0700

    trying rollup but the config doesn't work yet

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import json from '@rollup/plugin-json'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
+import terser from '@rollup/plugin-terser'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+export default {
+  input: 'dist/index.js',
+  output: {
+    inlineDynamicImports: true,
+    dir: 'bundle',
+    format: 'umd',
+    name: 'GenericSeqPanel',
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
+  },
+  plugins: [
+    nodeResolve({ browser: true }),
+    commonjs(),
+    json(),
+    nodePolyfills(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    terser(),
+  ],
+  external: ['react', 'react-dom'],
+}
