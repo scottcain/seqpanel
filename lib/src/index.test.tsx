7b2d08c15951027bbac4c7798b3891e7430da17a
Colin <colin.diesh@gmail.com>
2023-05-02 15:08:07 -0600

    Move stuff around for monorepo

--- lib/src/index.test.tsx
+++ lib/src/index.test.tsx
@@ -1,5 +1,6 @@
+import React from "react";
+
 import { render } from "@testing-library/react";
-import { describe, test } from '@jest/globals';
 import GenericSeqPanel from "./components/GenericSeqPanel";
 
 test("expect protein rendering", async () => {
