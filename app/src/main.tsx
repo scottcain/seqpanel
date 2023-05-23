1d6373c3ec3ee7447df18a8e1ffb1afe050c5ba1
Scott Cain <scott+git@scottcain.net>
2023-05-23 13:43:58 -0700

    eliminating the react import since it's not used here

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Buffer } from "buffer";
