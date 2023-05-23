7407f257a6f660d99a0e44713387fefd4ecfe5dd
Scott Cain <scott+git@scottcain.net>
2023-05-23 14:10:14 -0700

    switched to a gene with lots isoforms (etr-1)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,10 +3,10 @@ import { GenericGeneSeqPanel } from "generic-sequence-panel";
 export default function App() {
   return (
     <GenericGeneSeqPanel
-      refseq="X"
-      start={13201770}
-      end={13216729}
-      gene="WBGene00006749"
+      refseq="II"
+      start={162374}
+      end={170631}
+      gene="WBGene00001340"
       nclistbaseurl="https://s3.amazonaws.com/agrjbrowse/MOD-jbrowses/WormBase/WS288/c_elegans_PRJNA13758/"
       urltemplate="tracks/Curated_Genes/{refseq}/trackData.jsonz"
       fastaurl="https://s3.amazonaws.com/wormbase-modencode/fasta/current/c_elegans.PRJNA13758.WS284.genomic.fa.gz"
