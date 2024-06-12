ef065706f1939a18aeaccca9b78465c4f637a687
Colin <colin.diesh@gmail.com>
2024-06-12 17:15:49 -0400

    Fix useEffect

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,33 @@
-import GenericGeneSeqPanel from "generic-sequence-panel";
-import fetchTranscripts from "generic-sequence-panel";
+import { GenericGeneSeqPanel, fetchTranscripts } from "generic-sequence-panel";
+import { useEffect, useState } from "react";
 
-export default async function App() {
-  try {
-    const res = await fetchTranscripts({
-      nclistbaseurl: "https://s3.amazonaws.com/agrjbrowse/docker/7.0.0/human/",
-      fastaurl: "https://s3.amazonaws.com/agrjbrowse/fasta/sarscov2.fasta.gz",
-      refseq: "X",
-      start: 31097677,
-      end: 33339609,
-      gene: "DMD",
-      urltemplate: "tracks/All_Genes/{refseq}/trackData.jsonz",
-    });
-    console.log(res);
-  } catch (e) {
-    console.error(e);
-  }
+export default function App() {
+
+  const [result, setResult] = useState<unknown>()
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetchTranscripts({
+          nclistbaseurl: "https://s3.amazonaws.com/agrjbrowse/docker/7.0.0/human/",
+          fastaurl: "https://s3.amazonaws.com/agrjbrowse/fasta/sarscov2.fasta.gz",
+          refseq: "X",
+          start: 31097677,
+          end: 33339609,
+          gene: "DMD",
+          urltemplate: "tracks/All_Genes/{refseq}/trackData.jsonz",
+        });
+        console.log(res);
+        setResult(res)
+      } catch (e) {
+        console.error(e);
+      }
+
+    })()
+  }, [])
 
   return (
     <div>
+      {!result ? <div>Loading example of the fetchTranscripts function...</div> : <div>Got result from fetchTranscripts: {JSON.stringify(result).slice(0, 100)} etc. etc...</div>}
       <GenericGeneSeqPanel
         refseq="NC_045512.2"
         start={8555}
