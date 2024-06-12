305e67013b5ff1659e5d72b1f01bc04570b44166
Colin <colin.diesh@gmail.com>
2024-06-12 17:19:36 -0400

    Format

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,14 +2,15 @@ import { GenericGeneSeqPanel, fetchTranscripts } from "generic-sequence-panel";
 import { useEffect, useState } from "react";
 
 export default function App() {
-
-  const [result, setResult] = useState<unknown>()
+  const [result, setResult] = useState<unknown>();
   useEffect(() => {
     (async () => {
       try {
         const res = await fetchTranscripts({
-          nclistbaseurl: "https://s3.amazonaws.com/agrjbrowse/docker/7.0.0/human/",
-          fastaurl: "https://s3.amazonaws.com/agrjbrowse/fasta/sarscov2.fasta.gz",
+          nclistbaseurl:
+            "https://s3.amazonaws.com/agrjbrowse/docker/7.0.0/human/",
+          fastaurl:
+            "https://s3.amazonaws.com/agrjbrowse/fasta/sarscov2.fasta.gz",
           refseq: "X",
           start: 31097677,
           end: 33339609,
@@ -17,17 +18,23 @@ export default function App() {
           urltemplate: "tracks/All_Genes/{refseq}/trackData.jsonz",
         });
         console.log(res);
-        setResult(res)
+        setResult(res);
       } catch (e) {
         console.error(e);
       }
-
-    })()
-  }, [])
+    })();
+  }, []);
 
   return (
     <div>
-      {!result ? <div>Loading example of the fetchTranscripts function...</div> : <div>Got result from fetchTranscripts: {JSON.stringify(result).slice(0, 100)} etc. etc...</div>}
+      {!result ? (
+        <div>Loading example of the fetchTranscripts function...</div>
+      ) : (
+        <div>
+          Got result from fetchTranscripts:{" "}
+          {JSON.stringify(result).slice(0, 100)} etc. etc...
+        </div>
+      )}
       <GenericGeneSeqPanel
         refseq="NC_045512.2"
         start={8555}
--- lib/src/index.tsx
+++ lib/src/index.tsx
@@ -2,6 +2,6 @@ import GenericSeqPanel from "./components/GenericSeqPanel";
 import GenericGeneSeqPanel from "./components/GenericGeneSeqPanel";
 import fetchTranscripts from "./fetchTranscripts";
 
-console.log({ GenericGeneSeqPanel })
+console.log({ GenericGeneSeqPanel });
 
 export { GenericSeqPanel, GenericGeneSeqPanel, fetchTranscripts };
